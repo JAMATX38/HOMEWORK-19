5f5e1336788da0771db7181d28d211b25e462390
JAMATX38 <JAIMEMTX38@GMAIL.COM>
2021-12-13 15:51:35 -0600

    fixed serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,15 +1,6 @@
 // need to add files to cache
 // need the cache name- look at lesson 12
 // data cache- look at lesson 12
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/dist/app.bundle.js",
-  "/manifest.json",
-  "/service-worker.js",
- 
-];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
@@ -18,12 +9,14 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/",
     "index.html",
-    "/manifest.webmanifest",
+    "/manifest.json",
     "/styles.css",
     "/db.js",
     "/index.js",
     "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    "/icons/icon-512x512.png",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+
 ];
 
 
@@ -36,51 +29,63 @@ self.addEventListener("install", function(evt) {
       })
     );
   
-    self.skipWaiting();
+    // self.skipWaiting();
 });
 
 
 
 
-// activate pulled from lesson 12
-self.addEventListener("activate", (event) => {
-  const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
-        );
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
+// // activate pulled from lesson 12
+// self.addEventListener("activate", (event) => {
+//   const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+//   event.waitUntil(
+//     caches
+//       .keys()
+//       .then((cacheNames) => {
+//         return cacheNames.filter(
+//           (cacheName) => !currentCaches.includes(cacheName)
+//         );
+//       })
+//       .then((cachesToDelete) => {
+//         return Promise.all(
+//           cachesToDelete.map((cacheToDelete) => {
+//             return caches.delete(cacheToDelete);
+//           })
+//         );
+//       })
+//       .then(() => self.clients.claim())
+//   );
+// });
 
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
+  if (event.request.url.includes("/api/")) {
     event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(DATA_CACHE_NAME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+      caches.open(DATA_CACHE_NAME).then((cachedResponse) => {
+        return fetch(event.request)
+        .then((response) => {
+          if (response.status === 200) {
+            cachedResponse.put(event.request.url, response.clone())
+          }
+          return response
+        }).catch(err => {
+          return cachedResponse.match(event.request)
+        })
       })
-    );
-  }
-});
+      .catch(err => {
+        console.log(err)
+      })
+    )
+      return;
+    }
+
+  event.respondWith(fetch(event.request).catch(()=>{
+    return caches.match(event.request).then((res)=>{
+if(res){
+  return res;
+} else if (event.request.headers.get("accept").includes("text/html")){
+  return caches.match("/");
+}
+    })
+  }))
+      })
